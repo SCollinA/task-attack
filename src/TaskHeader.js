e2e849c7398cd7e2a0b4213851fe08fa0503f2d4
Collin Argo <collinargo@gmail.com>
2019-02-13 08:26:22 -0500

    do not link title if not logged in

--- src/TaskHeader.js
+++ src/TaskHeader.js
@@ -4,15 +4,17 @@ import TaskDoor from './TaskDoor'
 
 export default function TaskHeader({ username, isLoggedIn, goHome, selectUser, isUpdating, logout }) {
     return (
-        <div className='TaskHeader'>
+        <div className={`TaskHeader${isLoggedIn ? ' loggedInHeader' : ''}`}>
             {isLoggedIn && (
-                <div className={`updateUserIcon${isUpdating ? ' updatingUser' : ''}`} onClick={selectUser}>
+                <div className={`updateUserIcon${isUpdating ? ' updatingUser' : ''}`} 
+                    onClick={selectUser}
+                >
                     {/* <FontAwesomeIcon id='closedDoorIcon' size='2x' icon={['fas', 'door-closed']}/> */}
                     <FontAwesomeIcon id='userIcon' size='2x' icon={['fas', 'user-astronaut']}/>
                     <h6>{username}</h6>
                 </div>
             )}
-            <h1 onClick={goHome}>TaskAttack</h1>
+            <h1 onClick={isLoggedIn && goHome}>TaskAttack</h1>
             {isLoggedIn && (
                 <div className='logoutIcon'
                     onClick={() => isLoggedIn && logout()}
