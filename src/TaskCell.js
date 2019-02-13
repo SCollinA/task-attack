d6b04415b5c451780b751d90e915edf0b4eeef23
Collin Argo <collinargo@gmail.com>
2019-02-13 09:17:24 -0500

    size by time

--- src/TaskCell.js
+++ src/TaskCell.js
@@ -43,6 +43,7 @@ export default class TaskCell extends React.Component {
     render() {
         const { task, selectTask, selectedTask, updateTask, deleteTask } = this.props
         const isSelected = selectedTask && selectedTask.id === task.id
+        const taskTime = this.getTaskTime(task)
         return (
             <div className='TaskCellWrapper'>
                 <div className={`TaskCell${isSelected ? ' selectedTask' : ''}${task.active ? ' activeTask' : ''}`} 
@@ -51,11 +52,11 @@ export default class TaskCell extends React.Component {
                     // double click toggles active status on task
                     onDoubleClick={() => updateTask({ ...task, active: !task.active })}
                     style={{
-                        // height: `
-                        //     ${(task.time_end.slice(0, 2) * 60 + task.time_end.slice(2, 4)) - 
-                        //         (task.time_start.slice(0, 2) * 60 + task.time_start.slice(2, 4))}px
-                        // `,
-                        // minHeight: `40px`,
+                        height: `
+                            ${(taskTime.end[0] * 60 + taskTime.end[1]) - 
+                                (taskTime.start[0] * 60 + taskTime.start[1])}px
+                        `,
+                        minHeight: `40px`,
                     }}
                 >
                     <h6>{this.state.task.time_start}</h6>
