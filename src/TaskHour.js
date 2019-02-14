85cbe1dc942544bb0de64dc1bd4f4075d7564f98
Collin Argo <collinargo@gmail.com>
2019-02-14 13:56:08 -0500

    style time

--- src/TaskHour.js
+++ src/TaskHour.js
@@ -2,14 +2,14 @@ import React from 'react'
 import TaskCell from './TaskCell'
 
 export default function TaskHour({ tasks, hour, selectTask, selectedTask, addTask, updateTask, timeIsTaken, deleteTask }) {
-    // const zoomLevel = 1
+    const zoomLevel = 1
     return (
         <div className='taskHour'
-            // style={{
-            //     height: `
-            //         ${zoomLevel * 100}px
-            //     `
-            // }}
+            style={{
+                height: `
+                    ${zoomLevel * 100}px
+                `
+            }}
         >
             <div className='taskHourTime'>
                 <h1>{hour > 9 ? hour : `0${hour}`}:00</h1>
