3b85e2ee5e31fafb8462963680f664db5cd53eca
Collin Argo <collinargo@gmail.com>
2019-02-14 13:47:53 -0500

    add task hour file

---
+++ src/TaskHour.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import TaskCell from './TaskCell'
+
+export default function TaskHour({ tasks, hour, selectTask, selectedTask, addTask, updateTask, timeIsTaken, deleteTask }) {
+    // const zoomLevel = 1
+    return (
+        <div className='taskHour'
+            // style={{
+            //     height: `
+            //         ${zoomLevel * 100}px
+            //     `
+            // }}
+        >
+            <div className='taskHourTime'>
+                <h1>{hour > 9 ? hour : `0${hour}`}:00</h1>
+            </div>
+            {tasks.reverse().map(task => (
+                <TaskCell key={task.id || 
+                        Math.floor(Math.random() * Math.pow(tasks.length, 10))}
+                    hour={hour}
+                    task={task} 
+                    selectTask={selectTask} 
+                    selectedTask={selectedTask}
+                    addTask={addTask}
+                    updateTask={updateTask}
+                    timeIsTaken={timeIsTaken}
+                    deleteTask={deleteTask}
+                />
+            ))}
+        </div>
+    )
+}
